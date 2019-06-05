0ad6636157cc5b1272cdb07c844184dbd5ceadc1
typeness <jerylee1997@gmail.com>
2019-06-05 22:48:36 +0200

    Fix test after last commit

--- test/bash/issue4.sh
+++ test/bash/issue4.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 # https://github.com/malyvsen/steganosaurus/issues/4
 
-output="error: Carrier and output could not be the same file"
-
 
 echo -n "- the same filename for carrier and output in write mode: "
 echo "secret" >> data
 cp image.jpg image2.jpg
-if [[ $(./steganosaurus -m write -d data -c image2.jpg -o image2.jpg) =~ "$output" ]]; then
+./steganosaurus -m write -d data -c image2.jpg -o image2.jpg
+if [[ $? = 0 ]]; then
     echo "pass"
 else
     echo "fail"
@@ -18,7 +17,8 @@ rm data image2.jpg output 2> /dev/null
 echo -n "- the same filename for carrier and output in read mode: "
 echo "secret" >> data
 ./steganosaurus -m write -d data -c image.jpg -o image2.jpg
-if [[ $(./steganosaurus -m read -c image2.jpg -o image2.jpg) =~ "$output" ]]; then
+./steganosaurus -m read -c image2.jpg -o image2.jpg
+if [[ $? = 0 ]]; then
     echo "pass"
 else
     echo "fail"
@@ -29,7 +29,8 @@ rm data image2.jpg output 2> /dev/null
 echo -n "- the same filename for carrier and output in clear mode: "
 echo "secret" >> data
 ./steganosaurus -m write -d data -c image.jpg -o image2.jpg
-if [[ $(./steganosaurus -m clear -c image2.jpg -o image2.jpg) =~ "$output" ]]; then
+./steganosaurus -m clear -c image2.jpg -o image2.jpg
+if [[ $? = 0 ]]; then
     echo "pass"
 else
     echo "fail"
