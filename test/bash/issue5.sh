6a47cb750fe8ec15aaa9233cce832048b00c882e
typeness <jerylee1997@gmail.com>
2019-06-05 23:05:45 +0200

    Fix issue5.sh test

--- test/bash/issue5.sh
+++ test/bash/issue5.sh
@@ -7,8 +7,8 @@ test () {
     command=$3
     output=$4
     echo -n "$name "
-    touch data carrier
-    $pre
+    touch data
+    $pre 2> /dev/null
     if [[ $($command) =~ "$output" ]]; then
         echo "pass"
     else
@@ -19,14 +19,14 @@ test () {
 
 test "- error when could not open carrier file in write mode: " "rm carrier" "./steganosaurus -m write -c carrier -d data -o output" "error: carrier couldn't be opened"
 
-test "- error when could not open data file in write mode: " "rm data" "./steganosaurus -m write -d data -c carrier -o output" "error: data couldn't be opened"
+test "- error when could not open data file in write mode: " "rm data" "./steganosaurus -m write -d data -c image.jpg -o output" "error: data couldn't be opened"
 
-test "- error when could not open output file in write mode: " "" "./steganosaurus -m write -d data -c carrier -o error/output" "error: error/output couldn't be opened"
+test "- error when could not open output file in write mode: " "" "./steganosaurus -m write -d data -c image.jpg -o error/output" "error: error/output couldn't be opened"
 
 test "- error when could not open carrier file in read mode: " "rm carrier" "./steganosaurus -m read -c carrier -o output" "error: carrier couldn't be opened"
 
-test "- error when could not open output file in read mode: " "" "./steganosaurus -m read -c carrier -o error/output" "error: error/output couldn't be opened"
+test "- error when could not open output file in read mode: " "" "./steganosaurus -m read -c image.jpg -o error/output" "error: error/output couldn't be opened"
 
 test "- error when could not open carrier file in clear mode: " "rm carrier" "./steganosaurus -m clear -c carrier -o output" "error: carrier couldn't be opened"
 
-test "- error when could not open output file in clear mode: " "" "./steganosaurus -m clear -c carrier -o error/output" "error: error/output couldn't be opened"
+test "- error when could not open output file in clear mode: " "" "./steganosaurus -m clear -c image.jpg -o error/output" "error: error/output couldn't be opened"
