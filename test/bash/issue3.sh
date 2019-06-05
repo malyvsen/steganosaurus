02dad9a7d07f969d1272de8a4ae93aef512fe30f
typeness <jerylee1997@gmail.com>
2019-06-05 16:32:28 +0200

    Add regression test for #3

---
+++ test/bash/issue3.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# See https://github.com/malyvsen/steganosaurus/issues/3
+
+
+echo -n "- user decode file without message: "
+output="No data encoded"
+if [[ $(./steganosaurus read image.jpg output) =~ "$output" ]]; then
+    echo "pass"
+else
+    echo "fail"
+fi
+
+rm output 2> /dev/null
