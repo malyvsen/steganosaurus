b2cdf3279de8601a30f209335ca3930166ca03a5
malyvsen <5940672+malyvsen@users.noreply.github.com>
2019-06-05 22:45:12 +0200

    fix not-jpeg test to account for updated error message

--- test/bash/not-jpeg.sh
+++ test/bash/not-jpeg.sh
@@ -4,7 +4,7 @@
 echo -n "- error on non-jpeg file: "
 
 echo "secret" >> data
-output="error: image.png file is not JPG, or is corrupted"
+output="error: image.png is not JPG, or is corrupted"
 if [[ $(./steganosaurus -m write -o output.jpg -c image.png -d data) =~ "$output" ]]; then
     echo "pass"
 else
