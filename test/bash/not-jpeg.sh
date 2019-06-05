643a90729493c03c8344ec12783119edf07706e7
typeness <jerylee1997@gmail.com>
2019-06-05 19:10:24 +0200

    Add test case for not a JPEG file

---
+++ test/bash/not-jpeg.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# not a JPEG file
+echo -n "- error on non-jpeg file: "
+
+echo "secret" >> data
+output="error: image.png file is not JPG, or is corrupted"
+if [[ $(./steganosaurus -m write -o output.jpg -c image.png -d data) =~ "$output" ]]; then
+    echo "pass"
+else
+    echo "fail"
+fi
+rm data output.jpg 2> /dev/null
